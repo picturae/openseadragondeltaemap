986940be6bf8bfe4c4e3f1e53cd7d3666f7af124
ovanderzee <hastalavista@dds.nl>
2020-08-17 09:19:22 +0200

    remove redundant function

--- src/storage.js
+++ src/storage.js
@@ -1,7 +1,6 @@
 import { hasOwnProperty } from 'my-lib'
 
 const storage = new WeakMap()
-const userKeys = ['name', 'observed', 'assessed', 'reference', 'validity']
 
 /**
  * Store reference to data for a drawn rectangle
@@ -27,22 +26,4 @@ const getData = function(deltaeElement) {
     return null
 }
 
-/**
- * Retrieve user consumable data for a drawn rectangle
- * @private
- * @param {Object} userElement - HTMLElement
- * @return {Object} userData - all available data
- */
-const getUserData = function(deltaeElement) {
-    if (storage.has(deltaeElement)) {
-        const allData = storage.get(deltaeElement)
-        const userData = {}
-        userKeys.forEach(key => {
-            if (hasOwnProperty(allData, key)) userData[key] = allData[key]
-        })
-        return userData
-    }
-    return null
-}
-
-export { setData, getData, getUserData }
+export { setData, getData }
